73ac045ad840547cd152a1f424dfc3d151e635b9
Leonid Cupikov <dreambooms@gmail.com>
2021-06-29 11:47:02 +0300

    Implement Stats page

--- src/pages/stats-page/StatsPage.tsx
+++ src/pages/stats-page/StatsPage.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
+
 import './StatsPage.scss';
+import Comment, { IComment } from '../../components/comment/Comment';
+import reportWebVitals from '../../reportWebVitals';
 
 function StatsPage() {
+  const [comments, setComments] = useState<Array<IComment>>();
+  const [performance, setPerformace] = useState<{ [key: string]: number }>({});
+
+  useEffect(() => {
+    const commentsFetchStart = window.performance.now();
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(comments => {
+        const commentsFetch = window.performance.now() - commentsFetchStart;
+        setPerformace(performance => ({ ...performance, commentsFetch }))
+        setComments(comments);
+      });
+  }, []);
+
+  useEffect(() => {
+    reportWebVitals(data => {
+      setPerformace(performance => ({ ...performance, [data.name]: data.value }))
+    });
+  }, []);
+
   return (
-    <div>Stats page</div>
+    <section className="stats">
+      <div className="stats__data">
+        {Object.keys(performance).map(name => <div className="stats__item" key={name}>
+          <div className="stats__name">{name}</div>
+          <div className="stats__value">{performance[name]}ms</div>
+        </div>)}
+      </div>
+
+      <div className="data">
+        {!comments ? <div className="data__loading">Loading...</div> : <div className="data__content">
+          {comments.map(comment => <Comment key={comment.id} comment={comment} />)}
+        </div>}
+      </div>
+    </section>
   );
 }
 
