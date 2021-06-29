e6e98882afaffe239d326d5d663b48dc7725d439
Leonid Cupikov <dreambooms@gmail.com>
2021-06-29 10:48:24 +0300

    Implement Data page

---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,26 @@
+import './Comment.scss';
+
+export interface IComment {
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface CommentProps {
+  comment: IComment;
+}
+
+function Comment({ comment }: CommentProps) {
+  return (
+    <div className="comment">
+      <div className="comment__meta">
+        <div className="comment__name">{comment.name}</div>
+        <div className="comment__email">{comment.email}</div>
+      </div>
+      <div className="comment__body">{comment.body}</div>
+    </div>
+  );
+}
+
+export default Comment;
--- src/pages/data-page/DataPage.tsx
+++ src/pages/data-page/DataPage.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import './DataPage.scss';
+import Comment, { IComment } from '../../components/comment/Comment';
+
 
 function DataPage() {
+  const [comments, setComments] = useState<Array<IComment>>();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(setComments)
+  }, []);
+
   return (
-    <div>Data page</div>
+    <section className="data">
+      {!comments ? <div className="data__loading">Loading...</div> : <div className="data__content">
+        {comments.map(comment => <Comment key={comment.id} comment={comment} />)}
+      </div>}
+    </section>
   );
 }
 
